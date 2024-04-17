0c7d0a638dd654f4a915db6fbd1de692307da42b
milana-kurmanalieva <milanakurmanalieva23@gmail.com>
2024-04-17 18:32:26 +0600

    -iterator

--- src/redux/reducer.jsx
+++ src/redux/reducer.jsx
@@ -3,11 +3,11 @@ const initialState = {
 };
 
 export const options = [
-  { type: "INCREMENT", title: "+1", value: 1, iterator: "+" },
-  { type: "DECREMENT", title: "-1", value: 1, iterator: "-" },
-  { type: "RESET", title: "0", value: 0, iterator: "=" },
-  { type: "TEN", title: "+10", value: 10, iterator: "+" },
-  { type: "MINUS_TEN", title: "-10", value: 10, iterator: "-" },
+  { type: "INCREMENT", title: "+1", value: 1 },
+  { type: "DECREMENT", title: "-1", value: 1 },
+  { type: "RESET", title: "0", value: 0 },
+  { type: "TEN", title: "+10", value: 10 },
+  { type: "MINUS_TEN", title: "-10", value: 10 },
 ];
 
 export default function reducer(state = initialState, action) {
